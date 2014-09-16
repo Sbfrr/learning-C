4fe5ddf4a4bb535d1eb98074a0b40a83a609b971
sebastien <sebastien@ferre.io>
2014-09-16 14:24:12 +0200

    quick sort exercise solved

---
+++ algorithms/quicksort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/* Implement a basic quick sort in C*/
+
+void exchange(int array[], int a, int b)
+{
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
+
+void quickSort(int array[], int start, int end)
+{
+    int left = start-1;
+    int right = end+1;
+    const int pivot = array[start];
+
+    if(start >= end)
+        return;
+
+    while(1)
+    {
+        do right--; while(array[right] > pivot);
+        do left++; while(array[left] < pivot);
+
+        if(left < right)
+            exchange(array, left, right);
+        else break;
+    }
+
+    quickSort(array, start, right);
+    quickSort(array, right+1, end);
+}
+
+int main(void)
+{
+    int tab[6] = {8, 11, 3, 6, 9, 10};
+    int i;
+
+    quickSort(tab, 0, 4);
+
+    for(i = 0; i < 5; i++)
+    {
+      printf("%d ", tab[i]);
+    }
+    putchar('\n');
+
+    return 0;
+}
